75d26a587cb01c95c750f8cbe5a19c75a328dd57
Parth27 <diwanji.parth@gmail.com>
2020-10-25 20:12:52 -0400

    Fixed syntax and formatting

--- code/content.js
+++ code/content.js
@@ -1,12 +1,12 @@
 const generateBadge = (color, label, value) => {
-    let badgeDiv = document.createElement("div");
-    badgeDiv.setAttribute("id", label);
-    badgeDiv.style = `display: inline-block;
+  let badgeDiv = document.createElement("div");
+  badgeDiv.setAttribute("id", label);
+  badgeDiv.style = `display: inline-block;
                       border-radius: .75em;
                       font-family: 'Dejavu Sans','Arial';
                       margin-right: 20px;`;
-    let titleDiv = document.createElement("div");
-    titleDiv.style = `border-top-left-radius: .25em; 
+  let titleDiv = document.createElement("div");
+  titleDiv.style = `border-top-left-radius: .25em; 
                       border-bottom-left-radius: .25em;
                       background: #555555;
                       display: inline-block;
@@ -17,10 +17,10 @@ const generateBadge = (color, label, value) => {
                       margin: 0;
                       border: 0;
                       padding-left: 10px;
-                      padding-right: 10px;`
-    titleDiv.innerHTML = label;
-    let valueDiv = document.createElement("div");
-    valueDiv.style = `background: ${color};
+                      padding-right: 10px;`;
+  titleDiv.innerHTML = label;
+  let valueDiv = document.createElement("div");
+  valueDiv.style = `background: ${color};
                       border-top-right-radius: .25em;
                       border-bottom-right-radius: .25em;
                       display: inline-block;
@@ -32,82 +32,102 @@ const generateBadge = (color, label, value) => {
                       border: 0;
                       padding-left: 10px;
                       padding-right: 10px;`;
-    valueDiv.innerHTML = value;
-    badgeDiv.appendChild(titleDiv);
-    badgeDiv.appendChild(valueDiv);
-    return badgeDiv;
-}
+  valueDiv.innerHTML = value;
+  badgeDiv.appendChild(titleDiv);
+  badgeDiv.appendChild(valueDiv);
+  return badgeDiv;
+};
 
 const getExperience = (text) => {
-    return "3 years";
-}
+  return "3 years";
+};
 
 const getSponsorship = (text) => {
-    return "Yes";
-}
+  return "Yes";
+};
 
 const getDegree = (text) => {
-    return "MS Computer Science";
-}
+  return "MS Computer Science";
+};
 
 const getRemote = (text) => {
-    return "Yes";
-}
+  return "Yes";
+};
 
-function getElementByXpath(path,document) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+function getElementByXpath(path, document) {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
 }
 
 const displayBadge = (document) => {
-    const text = document.getElementsByTagName("article")[0].textContent;
+  const text = document.getElementsByTagName("article")[0].textContent;
 
-    let parentDiv = getElementByXpath("/html/body/div[8]/div[3]/div/div[1]/div[1]/div/div[1]/div/section/div[2]/div[1]",document);
-    if (parentDiv == null) {
-        parentDiv = getElementByXpath("/html/body/div[7]/div[3]/div/div[1]/div[1]/div/div[1]/div/section/div[2]/div[1]",document);
-    }
+  let parentDiv = getElementByXpath(
+    "/html/body/div[8]/div[3]/div/div[1]/div[1]/div/div[1]/div/section/div[2]/div[1]",
+    document
+  );
+  if (parentDiv == null) {
+    parentDiv = getElementByXpath(
+      "/html/body/div[7]/div[3]/div/div[1]/div[1]/div/div[1]/div/section/div[2]/div[1]",
+      document
+    );
+  }
 
-    const experienceBadge = generateBadge("#44cc11", "experience", getExperience(text));
-    const sponsorshipBadge = generateBadge("#00aadd", "sponsorship", getSponsorship(text));
-    const degreeBadge = generateBadge("#fa8128", "degree", getDegree(text));
-    const remoteBadge = generateBadge("#f20463", "remote", getRemote(text));
-    const badges = {
-        experience: experienceBadge,
-        sponsorship: sponsorshipBadge,
-        degree: degreeBadge,
-        remote: remoteBadge
-    };
-    return [parentDiv,badges];
-}
+  const experienceBadge = generateBadge(
+    "#44cc11",
+    "experience",
+    getExperience(text)
+  );
+  const sponsorshipBadge = generateBadge(
+    "#00aadd",
+    "sponsorship",
+    getSponsorship(text)
+  );
+  const degreeBadge = generateBadge("#fa8128", "degree", getDegree(text));
+  const remoteBadge = generateBadge("#f20463", "remote", getRemote(text));
+  const badges = {
+    experience: experienceBadge,
+    sponsorship: sponsorshipBadge,
+    degree: degreeBadge,
+    remote: remoteBadge,
+  };
+  return [parentDiv, badges];
+};
 
 function DelayedCount() {
   const [count, setCount] = useState(0);
+}
 
-  function handleClickAsync() {
-    setTimeout(function delay() {
-      setCount(count => count + 1);
-    }, 1000);
-  }
+function handleClickAsync() {
+  setTimeout(function delay() {
+    setCount((count) => count + 1);
+  }, 1000);
+}
 
 chrome.runtime.onMessage.addListener(newMessage);
 
-function newMessage(message, sender, sendResponse){
-    console.log("Message received");
-    resetBadges(message,document);
+function newMessage(message, sender, sendResponse) {
+  console.log("Message received");
+  resetBadges(message, document);
 }
 
-function resetBadges(message,document){
-    let [parentDiv,badges] = displayBadge(document);
-    for(const badge in message){
-        const badgeElement = document.getElementById(badge);
-        if(message[badge]["checked"]){
-            if(!badgeElement){
-                parentDiv.appendChild(badges[badge]);
-            }
-        }
-        else{
-            if(badgeElement){
-                badgeElement.remove();
-            }
-        }
+function resetBadges(message, document) {
+  let [parentDiv, badges] = displayBadge(document);
+  for (const badge in message) {
+    const badgeElement = document.getElementById(badge);
+    if (message[badge]["checked"]) {
+      if (!badgeElement) {
+        parentDiv.appendChild(badges[badge]);
+      }
+    } else {
+      if (badgeElement) {
+        badgeElement.remove();
+      }
     }
+  }
 }
--- code/popup.js
+++ code/popup.js
@@ -1,66 +1,66 @@
 let params = {
-    active: true,
-    currentWindow: true
+  active: true,
+  currentWindow: true,
 };
 
 let message = {
-    experience: {
-        checked: true
-    },
-    sponsorship: {
-        checked: true
-    },
-    degree: {
-        checked: true
-    },
-    remote: {
-        checked: true
-    }
+  experience: {
+    checked: true,
+  },
+  sponsorship: {
+    checked: true,
+  },
+  degree: {
+    checked: true,
+  },
+  remote: {
+    checked: true,
+  },
 };
 
-document.body.onload = function() {
-    chrome.storage.sync.get("data", function(items) {
-      if (!chrome.runtime.error) {
-        message = items["data"];
-        message = {
-            experience: {
-                checked: true
-            },
-            sponsorship: {
-                checked: true
-            },
-            degree: {
-                checked: true
-            },
-            remote: {
-                checked: true
-            }
-        };
-        for(const badge in message){
-            document.getElementById(badge).checked = message[badge]["checked"];
-        }
-        chrome.tabs.query(params, gotTabs);
+document.body.onload = function () {
+  chrome.storage.sync.get("data", function (items) {
+    if (!chrome.runtime.error) {
+      message = items["data"];
+      message = {
+        experience: {
+          checked: true,
+        },
+        sponsorship: {
+          checked: true,
+        },
+        degree: {
+          checked: true,
+        },
+        remote: {
+          checked: true,
+        },
+      };
+      for (const badge in message) {
+        document.getElementById(badge).checked = message[badge]["checked"];
       }
-    });
-  }
+      chrome.tabs.query(params, gotTabs);
+    }
+  });
+};
 
-function gotTabs(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, message);
+function gotTabs(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, message);
 }
 
-function toggle(event){
-    const badge = event.target.id;
-    message[badge]['checked'] = event.target.checked;
-    chrome.tabs.query(params, gotTabs);
-    chrome.storage.sync.set({ "data" : message }, function() {
-        if (chrome.runtime.error) {
-          console.log("Runtime error.");
-        }
-    });
+function toggle(event) {
+  const badge = event.target.id;
+  message[badge]["checked"] = event.target.checked;
+  chrome.tabs.query(params, gotTabs);
+  chrome.storage.sync.set({ data: message }, function () {
+    if (chrome.runtime.error) {
+      console.log("Runtime error.");
+    }
+  });
 }
 
 const checkboxes = document.getElementsByTagName("input");
 
-for(const checkbox of checkboxes){
-    checkbox.addEventListener('change', toggle, false);
+for (const checkbox of checkboxes) {
+  checkbox.addEventListener("change", toggle, false);
 }
