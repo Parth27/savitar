0a4486a2dc8d09930e9318d9f723338986f05be5
riddhi183 <64767985+riddhi183@users.noreply.github.com>
2020-10-28 11:34:07 -0400

    Updated comments

--- code/content_modules.js
+++ code/content_modules.js
@@ -1,5 +1,10 @@
 // Functions for parsing content for badges. Used globalThis to make these functions accessible in content.js
 globalThis.getExperience = (text) => {
+    /*
+    Input: Job Description 
+    Parses the job description to find the maximum number of years of experience required.
+    Return: Maximum number of years of job experience required
+    */
     var pattern = /[0-9]*[\s]*[\-]*[\s]*[0-9]+[\+]*[\s]*[or\smore]*[\s]*years/g;
     var result = text.match(pattern);
     var maxNum = parseInt("0")
@@ -23,6 +28,12 @@ globalThis.getExperience = (text) => {
 };
 
 globalThis.getSponsorship = (text) => {
+    /*
+    Input: Job Description 
+    Parses the job description to find if company provides sponsorship or not.
+    Return: Yes or Available
+    */
+
     var pattern = /(U[/.]*S Citizens|No sponsorship|No Sponsorship)/g;
     var result = text.match(pattern);
     if (result == null){
@@ -34,6 +45,11 @@ globalThis.getSponsorship = (text) => {
 };
 
 globalThis.getDegree = (text) => {
+    /*
+    Input: Job Description 
+    Parses the job description what degree is required.
+    Return: Degree requirement(e.g. Masters, Bachelors)
+    */
     var pattern = /(Master[\'s]*[\s]+|Bachelor[\'s]*[\s]*|M[\.]*s[\s]+|B[\.]*S[\s]+|BA[\s]*|Postdoctoral[\s]+|PhD[\s]+)/g;
 
     var result = text.match(pattern);
@@ -45,6 +61,11 @@ globalThis.getDegree = (text) => {
 };
 
 globalThis.getRemote = (text) => {
+    /*
+    Input: Job Description 
+    Parses the job description to know if remote work is allowed.
+    Return: Yes or N/A 
+    */
     var pattern = /(work from home|remote work)/g;
     var result = text.match(pattern);
     if (result == null){
